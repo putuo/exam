aa7735d424c5850a8c52d9551ba9fb7b10cb6558
putuo <putuo_0701@163.com>
2019-10-28 16:09:58 +0800

    第六题

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+
+function InputNumber(props) { 
+	return (		
+	 	<input type='number' {...props} onChange={props.onChange} />
+	)
+}
+
+
+
+function App() {
+	const [value, setValue] = useState('111')
+	return (
+		<div>
+			{/* <div>value值：{value}</div> */}
+
+		 
+			<InputNumber value={value} onChange={e => { 
+				setValue(e.target.value)
+			}} />
+
+		 
+			<InputNumber defaultValue={value} onChange={e => {
+				 console.log(e.target.value);
+			}} />
+		</div>
+	)
+}
+
+
+
+export default App
