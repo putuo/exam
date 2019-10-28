9ed2ca71c2288df2aa83d1814439150abf1aad53
putuo <putuo_0701@163.com>
2019-10-28 22:05:25 +0800

    第六题

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState ,Component} from 'react';
+import PropTypes from 'prop-types'
+ 
+class InputNumber extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			innerValue: ''
+		}
+	}
+	static propTypes = {
+		value: PropTypes.string,
+		onChange: PropTypes.func, 
+	}
+
+	static defaultProps = { 
+		onChange: () => {}
+	}
+
+	get isControl(){
+		return 'value' in this.props
+	}
+
+	get value() {
+		if(this.isControl){
+			return this.props.value
+		} else {
+			return this.state.innerValue
+		}
+	}
 
 
-function InputNumber(props) { 
-	return (		
-	 	<input type='number' {...props}  />
-	)
+	render() {
+		return (
+			<input type="number" value={this.value}
+
+			onChange={(e) => {
+				if(!this.isControl){
+					this.setState({
+						innerValue: e.target.value
+					})
+				}
+				this.props.onChange(e)
+			}}
+		/>
+		)
+	}
+
+	componentDidMount() {
+		this.setState({
+			innerValue: this.props.defaultValue
+		})
+	}
+
 }
+
+
+function InputNumber2(props) {
+    return (
+        <input type='number' {...props}  />
+    )
+}
+
+
 
 
 
@@ -14,7 +70,6 @@ function App() {
 	return (
 		<div>
 			<div>value值：{value}</div>
-
 		 
 			<InputNumber value={value} onChange={e => { 
 				setValue(e.target.value)
